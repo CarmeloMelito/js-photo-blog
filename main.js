ad4358bef4cf301a252efaff7f6f401a8383ce07
CarmeloMelito <melitocarmelo211@gmail.com>
2025-03-15 13:25:06 +0100

    feat: console array

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('ciao');
+let img = 'https:lanciweb.github.io/demo/api/pictures';
+let image = []
+
+axios.get(img)
+    .then(response => {
+        image.push(response.data)
+        console.log(image)
+
+
+
+    })
