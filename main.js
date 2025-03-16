c3524b770e61804c272f11018a4be017c66c8972
CarmeloMelito <melitocarmelo211@gmail.com>
2025-03-16 23:00:26 +0100

    feat:div in js

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 let img = 'https:lanciweb.github.io/demo/api/pictures';
-let image = []
+
+
+const cardContainer = document.querySelectorAll('cardContainer')
+//console.log(cardContainer);
 
 axios.get(img)
     .then(response => {
-        image.push(response.data)
-        console.log(image)
+        const addImage = response.data
+        console.log(addImage)
 
 
+        let image = [];
 
-    })
+        addImage.forEach(element => {
+            image += `<div class="card-container">
+    <div class="pin"><img src="img/pin.svg" alt="pin"></div>
+    <!-- <img src="img prova.jpg" alt="propva-img" > -->
+    <div class="description">
+        <h4></h4>
+    </div>
+</div>`;
+            console.log(image)
+        })
+    });
+
