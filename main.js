bfb5d76a136209af406de60cee9f13df05aa8e12
CarmeloMelito <melitocarmelo211@gmail.com>
2025-03-17 10:36:37 +0100

    feat:add image in js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let img = 'https:lanciweb.github.io/demo/api/pictures';
 
 
-const cardContainer = document.querySelectorAll('cardContainer')
-//console.log(cardContainer);
+const cardContainer = document.querySelector('.container')
+console.log(cardContainer);
 
 axios.get(img)
     .then(response => {
@@ -10,17 +10,27 @@ axios.get(img)
         console.log(addImage)
 
 
-        let image = [];
+        let image = '';
 
         addImage.forEach(element => {
-            image += `<div class="card-container">
-    <div class="pin"><img src="img/pin.svg" alt="pin"></div>
-    <!-- <img src="img prova.jpg" alt="propva-img" > -->
-    <div class="description">
-        <h4></h4>
-    </div>
-</div>`;
+            image += ` 
+        <div class="card-container">
+            <div class="pin"><img src="img/pin.svg" alt="pin"></div>
+        <img src="${element.url}" alt="${element.title}" style="width: 100%; height: 90%; object-fit: cover; ">
+            <div class="description">
+                 <h4>${element.title}</h4>
+                    <p>${element.date}</p>
+            </div>
+        </div>`;
             console.log(image)
+
         })
+        cardContainer.innerHTML = image
     });
+
+
+
+
+
+
 
