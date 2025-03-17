bfc894e4996f598ebac11c1862eaff6d0ef854ca
CarmeloMelito <melitocarmelo211@gmail.com>
2025-03-17 19:26:32 +0100

    feat:click image

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let img = 'https:lanciweb.github.io/demo/api/pictures';
 
 const cardContainer = document.querySelector('.container')
 console.log(cardContainer);
+// AXIOS
 
 axios.get(img)
     .then(response => {
@@ -16,13 +17,13 @@ axios.get(img)
             image += ` 
         <div class="card-container">
             <div class="pin"><img src="img/pin.svg" alt="pin"></div>
-        <img src="${element.url}" alt="${element.title}" style="width: 100%; height: 90%; object-fit: cover; ">
+        <img  src="${element.url}" alt="${element.title}" style="width: 100%; height: 90%; object-fit: cover; ">
             <div class="description">
                     <p>${element.date}</p>
                      <h4>${element.title}</h4>
             </div>
         </div>`;
-            // console.log(image)
+            // console.log(image);
         })
         cardContainer.innerHTML = image
     });
@@ -31,17 +32,28 @@ axios.get(img)
 
 
 const overlay = document.querySelector('.button');
-console.log(overlay);
+//console.log(overlay);
 
 const overlayImage = document.getElementById('overlay-image')
-console.log(overlayImage);
+//console.log(overlayImage);
 
 const buttonOver = document.getElementById('button-over');
-console.log(buttonOver);
+//console.log(buttonOver);
 
+// IMMAGINE
+document.querySelector('.container').addEventListener('click', (element) => {
+    overlay.classList.remove('hidden');
+    console.log('ho clickato l immagine');
+}
+);
+
+
+//Bottone
 buttonOver.addEventListener('click', () => {
     console.log('ho clickato il bottone')
+    overlay.classList.remove('hidden');
 })
+
 
 
 
